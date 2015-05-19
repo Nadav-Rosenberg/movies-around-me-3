268d51a4aed9ca43f938b8604e844c7bf535c30f
Benjamin Kollek <benjaminkollek@Benjamins-MacBook-Pro.local>
2015-05-19 19:31:32 +0100

    Added controller to index.html

---
+++ public/app.js
@@ -0,0 +1,7 @@
+var moviesAroundMe = angular.module('moviesAroundMe',[]);
+
+moviesAroundMe.controller('moviesControl', function() {
+  var self = this;
+
+  console.log('Helllloooooooooooooooo!');
+});

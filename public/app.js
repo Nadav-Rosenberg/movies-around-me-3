f441dc3282b2abbd4dcb01a4e65da803a6020465
Benjamin Kollek <benjaminkollek@Benjamins-MacBook-Pro.local>
2015-05-20 15:59:38 +0100

    Rating service displayed on index.html

--- public/app.js
+++ public/app.js
@@ -1,11 +1,15 @@
 var moviesAroundMe = angular.module('MoviesAroundMe',['ngResource']);
 
 moviesAroundMe.controller('moviesControl', ['OMDb', function(OMDb) {
-
+  self = this;
   omdbData: Object;
 
   var self = this;
-  OMDb.makeRequest('home').then(function(data) {
-    console.log(data);
-  });
+
+  self.updateMovieRating = function(){
+    console.log('Movie title: ' + self.movieTitle)
+    OMDb.makeRequest(self.movieTitle).then(function(response) {
+      self.imdbRating = response.data.imdbRating;
+    });
+  };
 }]);

7e5b31b4b299263c48f7986a67a78a38aaa6dd97
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-05-27 15:49:36 +0100

    refactors moviesList app

--- public/app.js
+++ public/app.js
@@ -5,19 +5,12 @@ moviesAroundMe.controller('moviesControl', ['OMDb',  'Moviesapi', function(OMDb,
   self.moviesList = []; // [{title: , cinema: , distance: , rating: }...];
   self.postcode;
 
-  // self.updateMovieRating = function(){
-  //   OMDb.getRating(self.movieTitle).then(function(response) {
-  //     self.imdbRating = response.data.imdbRating;
-  //   });
-  // };
 
   self.findMoviesAroundMe = function() {
     Moviesapi.getMovies(self.postcode, function(movies) {
       self.moviesList = movies;
       for(var i=0; i < movies.length; i++) {
         OMDb.getRating(movies[i].title.slice(0, -7)).then(function(response) {
-          console.log(response.data);
-          console.log(response.data.imdbRating);
           for(var i=0; i < movies.length; i++) {
             if (movies[i].title.slice(0,-7).toLowerCase() == response.data.Title.toLowerCase()) {
               if (response.data.imdbRating !== "N/A") {
@@ -30,5 +23,11 @@ moviesAroundMe.controller('moviesControl', ['OMDb',  'Moviesapi', function(OMDb,
     });
   };
 
+  // self.updateMovieRating = function(){
+  //   OMDb.getRating(self.movieTitle).then(function(response) {
+  //     self.imdbRating = response.data.imdbRating;
+  //   });
+  // };
+  
 }]);
 
--- public/services/moviesapi.js
+++ public/services/moviesapi.js
@@ -4,28 +4,33 @@ moviesAroundMe.factory('Moviesapi', ['$http', function($http) {
 
   var movies = [];
 
-    return {
-      getMovies: function(postcode, call_me_when_finished) {
+  var addMoviesFromCinema = function(response) {
+    var size = response.data.length;
+    for(var i=0; i < (size - 1); i++) {
+      movies.push({title: response.data[i].title, cinema: response.data[(size -1)].a, distance: response.data[(size -1)].b, rating: " "});                   
+    };
+  }
 
-        return $http.get('http://aqueous-badlands-8518.herokuapp.com/cinemas/find/' + postcode)
-          .then(function(response) { 
-            for(var i =0; i < response.data.length; i++) {
-
-                var cinema = response.data[i].address;
-                var distance = response.data[i].distance;
-
-                $http.get('http://aqueous-badlands-8518.herokuapp.com/cinemas/' + response.data[i].venue_id + '/showings' + '?a='+ cinema +'&b=' + distance)
-                .then(function(response) {
-                   var size = response.data.length;
-                   for(var i=0; i < (size - 1); i++) {
-                     movies.push({title: response.data[i].title, cinema: response.data[(size -1)].a, distance: response.data[(size -1)].b, rating: " "});                   
-                     call_me_when_finished(movies);
-                   };
-                  
-                });                
-            };
-           
-          });
-      },
+  return {
+    getMovies: function(postcode, updateMoviesList) {
+      return $http.get('http://aqueous-badlands-8518.herokuapp.com/cinemas/find/' + postcode)
+        .then(function(response) { 
+          for(var i =0; i < response.data.length; i++) {
+          var venue_id = response.data[i].venue_id
+          var address = response.data[i].address;
+          var distance = response.data[i].distance;
+          $http.get('http://aqueous-badlands-8518.herokuapp.com/cinemas/' + 
+                     venue_id + '/showings' + '?a=' + address + '&b=' + distance)
+            .then(function(response) {
+              addMoviesFromCinema(response);
+              updateMoviesList(movies);
+            });               
+        };         
+      });
     }
-  }]);
+  }
+
+
+}]);
+
+
